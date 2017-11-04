725110912e434146124356556c660e2d48d9cad8
naman93 <namankt55@gmail.com>
2017-11-03 20:46:06 -0400

    updated nn_train.py to add support for debug mode, used to test network design and hyper-parameters

--- nn_train.py
+++ nn_train.py
@@ -12,6 +12,11 @@ from nn_model import segnet_model1
 
 #flag to indicate debug mode
 debug = True
+#number of training samples to use in debug mode
+#the same set of samples will be used repeatedly in each epoch
+debug_train_count = 50
+#number of validation samples to use in debug mode
+debug_val_count = 5
 
 #setup matplotlib
 #set interactive mode on
@@ -19,14 +24,16 @@ plt.ion()
 plt.show()
 
 #hyper parameters
+#percentage of training data
+percent_training_data = 70
 #batch size
 batch_size = 16
 #learning rate
-eta = 0.1
+eta = 0.5
 #number of epochs
-num_epochs = 1000
+num_epochs = 100
 #create a data container, with 70% as training data and batch_size=16
-data = data_container(70, batch_size)
+data = data_container(percent_training_data, batch_size, debug)
 #neural network model
 net = segnet_model1()
 #optimizer
@@ -42,10 +49,11 @@ normalize = tv.transforms.Compose([tv.transforms.Normalize(5.9,21.54)])
 for epoch in range (num_epochs):
     #set data container mode to train
     data.set_mode('train')
-    error = 0
-    count = 0
+    train_error = 0
+    train_count = 0
     #iterate over the training samples
     for batch_id, (input_image_tensor, target_image_tensor) in enumerate(data):
+        #TODO : implement normalization of input images
         #normalize the input images
         #input_image_tensor = normalize(input_image_tensor)
         #pad the input images and convert to Variable
@@ -65,16 +73,20 @@ for epoch in range (num_epochs):
         #backpropagate the error
         err.backward()
         #compute the summation of the error for the batch (average error for batch * batch size)
-        error += (err.data[0] * input_image_tensor.size()[0])
-        count += input_image_tensor.size()[0]
+        train_error += (err.data[0] * input_image_tensor.size()[0])
+        train_count += input_image_tensor.size()[0]
+        #if in debug mode, stop training once we have looked at the required number of samples
+        if (debug):
+            if (train_count >= debug_train_count):
+                break
     #save the training error
-    error_dict['train'].append(error/count)
+    error_dict['train'].append(train_error/train_count)
 
     #set mode of data container to 'val'
     data.set_mode('val')
     #reset the vlues of count and error
-    error = 0
-    count = 0
+    val_error = 0
+    val_count = 0
     #iterate over the validation samples
     for index, (input_image_tensor, target_image_tensor) in enumerate(data):
         #pad the input images and convert to Variable
@@ -86,16 +98,22 @@ for epoch in range (num_epochs):
         #compute the error
         err = F.nll_loss(output, target_image_tensor.type(t.LongTensor)[:,0,:,:])
         #compute the summation of the error for each sample
-        error += (err.data[0])
-        count += 1
+        val_error += (err.data[0])
+        val_count += 1
+        #if in debug mode, stop validating once we have looked at the required number of samples
+        if (debug):
+            if (val_count >= debug_val_count):
+                break
     #save the validation error
-    error_dict['val'].append(error/count)
+    error_dict['val'].append(val_error/val_count)
+
     #update the weights
     optimizer.step()
 
-    #update plot
+    #update the plot
     if (debug):
         #plot the error values
+        #TODO: add a title to the graph and also a legend for the two plots
         plt.plot(error_dict['train'], 'xb-')
         plt.plot(error_dict['val'], 'r-')
         plt.pause(0.001)
